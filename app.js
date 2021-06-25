602ff40fd934137894d4b6dbb10a282259f5976d
Abdullah-MotiWala <motiwalaabdullah100@gmail.com>
2021-06-25 18:24:53 +0500

    name Cushion Added, all layout complete just responsiveness remaining

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+// Slider of Product Starts
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 40,
-    centeredSlides: true,
-    // grabCursor: true,
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 40,
+  centeredSlides: true,
+  // grabCursor: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+// Slider of Product Ends
 
-// Funciton For Div animation add
+// Funciton For cart animation add
 function addCart(){
   var nav = document.getElementById("ul");
   var cartDiv =document.getElementById("cart");
